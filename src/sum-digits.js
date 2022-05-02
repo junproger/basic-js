2ef3132e4c560be908252f1e0b1a9d0935126937
Junior Programmer <progphyton@gmail.com>
2022-05-03 02:47:13 +0300

    feat: add ready tasks

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,10 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+
+function getSumOfDigits(num) {
+	let res= 0;
+	let str= '' + num;
+	for (let chr of str) {
+		res += +chr;
+	};
+	if (res > 9) {
+		return getSumOfDigits(res);
+	} else {
+		return res;
+	};
+};
 
 module.exports = {
   getSumOfDigits
